be17bf8578e0e167a7222070042b3f3e82873556
Andre Gallo <9999415+afgallo@users.noreply.github.com>
2023-11-14 08:34:27 +1100

    change colorscheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "tokyonight-storm",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
